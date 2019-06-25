0acece09a8b962ee225c93842f94cc364aab2414
rramirez <rramirez@capmega.com>
2019-06-25 10:47:53 -0500

    Creacion de los archivos del ejercicio dos, asic como el boton para mandar a llamar la nueva activity

---
+++ app/src/main/java/google/rrp/capmegajava/EjercicioTwo.java
@@ -0,0 +1,21 @@
+package google.rrp.capmegajava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * Jun 2019
+ * Raúl Ramírez
+ * rramirez@capmega.com
+ */
+
+
+public class EjercicioTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio_two);
+    }
+}
