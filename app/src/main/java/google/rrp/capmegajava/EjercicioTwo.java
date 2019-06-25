70c7f1b8d82a229bd3d41c3190e044165575dca9
rramirez <rramirez@capmega.com>
2019-06-25 12:14:58 -0500

    Vista del layout ok y ya se observa el momneto de dar click en los radio button

--- app/src/main/java/google/rrp/capmegajava/EjercicioTwo.java
+++ app/src/main/java/google/rrp/capmegajava/EjercicioTwo.java
@@ -2,6 +2,11 @@ package google.rrp.capmegajava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /*
  * Capmega Soluciones Informáticas S.A.S de C.V
@@ -12,10 +17,20 @@ import android.support.v7.app.AppCompatActivity;
 
 
 public class EjercicioTwo extends AppCompatActivity {
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ejercicio_two);
+        radioGroup = findViewById(R.id.rdgp_suma_resta);
+
+    }
+    public void checButton(View view){
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+        Toast.makeText(this,"Seleccionaste la acción: "+radioButton.getText(), Toast.LENGTH_SHORT).show();
     }
 }
