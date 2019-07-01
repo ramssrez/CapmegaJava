2517c1b95c3c72a8b57f5ba8f3d7727bb1ebf3bc
rramirez <rramirez@capmega.com>
2019-07-01 13:41:58 -0500

    Lineas de mas

--- app/src/main/java/google/rrp/capmegajava/EjercicioFour.java
+++ app/src/main/java/google/rrp/capmegajava/EjercicioFour.java
@@ -3,7 +3,6 @@ package google.rrp.capmegajava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
