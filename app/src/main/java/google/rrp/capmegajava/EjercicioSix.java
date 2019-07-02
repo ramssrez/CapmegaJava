b3bbfe6f9b9249a70927646e0b7eddecf565b0ea
rramirez <rramirez@capmega.com>
2019-07-02 13:48:20 -0500

    Vista ok de lo que se desea en el ejercicio

--- app/src/main/java/google/rrp/capmegajava/EjercicioSix.java
+++ app/src/main/java/google/rrp/capmegajava/EjercicioSix.java
@@ -1,11 +1,12 @@
 package google.rrp.capmegajava;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 /*
  * Capmega Soluciones Informáticas S.A.S de C.V
@@ -14,9 +15,12 @@ import android.widget.Toast;
  * rramirez@capmega.com
  */
 public class EjercicioSix extends AppCompatActivity implements View.OnClickListener {
-    private Button btn_string, btn_int, btn_double ;
+    private Button btn_string, btn_int, btn_double;
+    final String[] strings_types = {"Spider", "Tiger", "Lion", "Bird", "Monster", "Cat", "Dog", "Duck", "Dragon", "Rat"};
+    final int[] enteros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    final double[] doubles = {1.1, 2.3, 5.9, 45.2, 458.6, 12.9, 15.9, 9.6, 5.1, 2.1};
 
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ejercicio_six);
         btn_string = findViewById(R.id.btn_string);
@@ -30,23 +34,32 @@ public class EjercicioSix extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @SuppressLint("LongLogTag")
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_string:
-                Toast.makeText(this, "Funcionamiento de los botones 1", Toast.LENGTH_SHORT).show();
-                Log.i("boton de los strings", String.valueOf(btn_string));
+                Log.i("El array tiene la siguiente dimensión ", String.valueOf(strings_types.length));
+                for (int i = 0; i < strings_types.length; i++) {
+                    Log.i("Array de los strings en la posicion: "+i, strings_types[i]);
+                }
                 break;
             case R.id.btn_int:
-                Toast.makeText(this, "Funcionamiento de los botones 2", Toast.LENGTH_SHORT).show();
-                Log.i("Boton de los enteros", String.valueOf(btn_int));
+                Log.i("El array tiene la siguiente dimensión ", String.valueOf(enteros.length));
+                for (int i = 0; i < enteros.length; i++) {
+                    Log.i("Array de los enteros en la posición: "+i, String.valueOf(enteros[i]));
+                }
                 break;
             case R.id.btn_double:
-                Toast.makeText(this, "Funcionamiento de los botones 3", Toast.LENGTH_SHORT).show();
-                Log.i("boton de los doubles", String.valueOf(btn_double));
+                Log.i("El array tiene la siguiente dimensión ", String.valueOf(doubles.length));
+                for (int i = 0; i < doubles.length; i++) {
+                    Log.i("Array de los doubles en la posicón: "+i, String.valueOf(doubles[i]));
+                }
                 break;
-
         }
-
     }
+
 }
+
+
+
